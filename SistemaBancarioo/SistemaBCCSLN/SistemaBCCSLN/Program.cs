5d8afbcbba76c17aba876a2ba274c6e23090e6e6
dayvisson <dayvisson2122@gmail.com>
2026-02-09 11:02:41 -0300

    fim

---
+++ SistemaBancarioo/SistemaBCCSLN/SistemaBCCSLN/Program.cs
@@ -0,0 +1,35 @@
+﻿using SistemaBCCSLN;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+
+       
+        Banco meuBanco = new Banco();
+
+        meuBanco.Nome = "Banco do Brasil";
+
+        meuBanco.CodigoBACEN = "001";
+
+
+       
+        Transacao minhaTransacao = new Transacao();
+
+        minhaTransacao.Valor = 250.00m;
+
+        minhaTransacao.Data = DateTime.Now;
+
+        minhaTransacao.Tipo = "PIX";
+
+        ContaBancaria CoontaBancaria = new ContaBancaria();
+
+        Console.WriteLine("    REGISTRO BACEN    ");
+        Console.WriteLine("Banco: " + meuBanco.Nome);
+        Console.WriteLine("Código: " + meuBanco.CodigoBACEN);
+        Console.WriteLine("----------------------");
+        Console.WriteLine("Tipo: " + minhaTransacao.Tipo);
+        Console.WriteLine("Valor: " + minhaTransacao.Valor);
+        Console.WriteLine("Data: " + minhaTransacao.Data);
+    }
+}
